a7b3200334aecd8afb763eb8a58c45e2bcd7dd16
shafnasiddeek <145668205+shafnasiddeek@users.noreply.github.com>
2023-10-13 16:00:58 +0530

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,3 @@
+dict_1={"appu":22,"ammu":33,"raju":35}
+del dict_1['ammu']
+print("the dictionary after remove is:",dict_1)
---
+++ sumdict.py
@@ -0,0 +1,5 @@
+dict={1:100,2:200,3:300,4:400,5:500}
+s=0
+for d in dict.values():
+    s=s+d
+    print(s)
