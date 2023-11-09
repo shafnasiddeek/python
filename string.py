7700a55daa7a9724fa55ddc3f5338e80328a42dd
shafnasiddeek <145668205+shafnasiddeek@users.noreply.github.com>
2023-11-09 16:31:31 +0530

    Add files via upload

--- string.py
+++ string.py
@@ -1,4 +1,6 @@
-s1=input("enter string 1\n")
-s2=input("enter string 2\n")
-s3=s2[0]+s1[1:]+' '+s1[0]+s2[1:]
-print("\n",s3)
+string=input("enter a word")
+ing="ing"
+if(string[-3:]=="ing"):
+    print(string+"ly")
+else:
+    print(string+"ing")
